22df5750c38ada48d33fdeea4d58abac24ef8468
Maciej <sienkiewicz.maciej971030@gmail.com>
2019-05-13 10:57:56 +0200

    Initial commit

---
+++ src/main/java/org/sienkiewicz/conferenceapp/ConferenceappApplication.java
@@ -0,0 +1,13 @@
+package org.sienkiewicz.conferenceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sienkiewicz/conferenceapp/ConferenceappApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sienkiewicz.conferenceapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConferenceappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
